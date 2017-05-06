1130ff4c07e95addaa863920ed28bc3591d61fdf
Yuya Minami <yuya373@me.com>
2017-05-07 01:35:37 +0900

    add backend

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+const child_process = require('child_process')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let backendServer
 
 function createWindow () {
   // Create the browser window.
@@ -38,6 +40,10 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+app.on('ready', () => {
+  backendServer = child_process.spawn('./backend-exe')
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
